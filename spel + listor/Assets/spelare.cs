78e73cc52b3250765402f9310347d97b5f535041
Michaelboi <micnwa050628@student.jenseneducation.se>
2022-12-13 13:40:11 +0100

    movement med class

--- spel + listor/Assets/spelare.cs
+++ spel + listor/Assets/spelare.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using spawnpositions;
+using spel_move;
 
 public class spelare : MonoBehaviour
 {
     player_spawns current_pos;
+    player_movement move;
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.SetActive(true);
         current_pos = GetComponent<player_spawns>();
         current_pos.position();
+        move.GetComponent<player_movement>();
+        move.kub_movement();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move.movement();   
     }
 }
