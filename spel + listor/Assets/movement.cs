9a46d0b6b9184f1328bc2f241edf17ad220056c0
nopplis <malnom050808@student.jenseneducation.se>
2022-12-13 14:09:32 +0100

    move

--- spel + listor/Assets/movement.cs
+++ spel + listor/Assets/movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< Updated upstream
+
 namespace player_move
 {
     public class movement : MonoBehaviour
@@ -62,44 +62,3 @@ namespace player_move
         }
     }
 }
-
-=======
-public class movement : MonoBehaviour
-{
-    Rigidbody RB;
-    int wholeNumber = 4;
-    float decimalNumber = 2.56f;
-    string text = "hejhej";
-    // Start is called before the first frame update
-    void Start()
-    {
-        RB = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
-        {
-            RB.velocity = new Vector3(0, 5f, 0);
-        }
-        if (Input.GetKey("up"))
-        {
-            RB.velocity = new Vector3(0, 0, 5f);
-        }
-        if (Input.GetKey("right"))
-        {
-            RB.velocity = new Vector3(5f, 0, 0);
-        }
-        if (Input.GetKey("down"))
-        {
-            RB.velocity = new Vector3(0, 0, -5f);
-        }
-        if (Input.GetKey("left"))
-        {
-            RB.velocity = new Vector3(-5f, 0, 0);
-        }
-
-    }
-}
->>>>>>> Stashed changes
--- spel + listor/Assets/spelare.cs
+++ spel + listor/Assets/spelare.cs
@@ -2,44 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using spawnpositions;
-<<<<<<< Updated upstream
 using player_move;
-=======
->>>>>>> Stashed changes
 
 public class spelare : MonoBehaviour
 {
     player_spawns current_pos;
-<<<<<<< Updated upstream
     movement move = new movement();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
         move = GetComponent<movement>();
         current_pos = GetComponent<player_spawns>();
         current_pos.position();
         move.RB = GetComponent<Rigidbody>();
-
-
-
-=======
-    // Start is called before the first frame update
-    void Start()
-    {
-        current_pos = GetComponent<player_spawns>();
-        current_pos.position();
->>>>>>> Stashed changes
+  
     }
-
     // Update is called once per frame
     void Update()
     {
-        
-<<<<<<< Updated upstream
         move.spel_movement();
-=======
->>>>>>> Stashed changes
+
     }
 }
